796abe6b7b911f38f8bbc24cecf240302c2d34ce
ncvictorious <nwaobi.victor@gmail.com>
2024-03-14 11:20:36 +0000

    added class

--- Test.java
+++ Test.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-    System.out.println("Test");
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }  
 }
